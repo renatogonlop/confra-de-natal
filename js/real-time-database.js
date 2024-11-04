6a593daede18c268ef577aa9232201540c6163f3
Renato Gonçalves <88465590+renatogonlop@users.noreply.github.com>
2024-11-04 16:59:21 -0300

    vr

--- js/real-time-database.js
+++ js/real-time-database.js
@@ -2,8 +2,8 @@
 const dataAtual = new Date();
 const anoAtual = dataAtual.getFullYear();
 let ano = getElementById('anoDate')
-
 ano.innerHTML = `Confraternização de Natal<br>${anoAtual}`
+
 
 // Ao clicar no botão
 function envia() {
@@ -79,8 +79,6 @@ function envia() {
 
 
 };
-
-
 
 //Abre/fecha blocos
 function yes() {
