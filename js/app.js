4a19df1cd707b6c7ed76ed615f2137de8247ea8f
Renato Gonçalves <88465590+renatogonlop@users.noreply.github.com>
2024-11-04 16:46:38 -0300

    VER.2

--- js/app.js
+++ js/app.js
@@ -1,13 +1,7 @@
 (function () {
 
     var config = {
-        apiKey: "AIzaSyDXO0b2Y-zMj7qpxTVwT1smkvJdT4QtxTA",
-        authDomain: "confra-de-natal-9eb54.firebaseapp.com",
-        databaseURL: "https://confra-de-natal-9eb54-default-rtdb.firebaseio.com",
-        projectId: "confra-de-natal-9eb54",
-        storageBucket: "confra-de-natal-9eb54.appspot.com",
-        messagingSenderId: "170076988427",
-        appId: "1:170076988427:web:a50acc6c04f3067692930e"
+       
     };
 
     // Initialize Firebase
